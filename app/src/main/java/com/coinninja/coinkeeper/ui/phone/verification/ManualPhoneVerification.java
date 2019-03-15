54640d8d123ebff0f503462e0c7e3c74daa91ef1
Michael Lemley <michael.lemley@coinninja.com>
2019-03-15 14:48:27 +0000

    show internationalized string representation of a contact in the invite help screen

--- app/src/main/java/com/coinninja/coinkeeper/ui/phone/verification/ManualPhoneVerification.java
+++ app/src/main/java/com/coinninja/coinkeeper/ui/phone/verification/ManualPhoneVerification.java
@@ -1,15 +1,16 @@
 package com.coinninja.coinkeeper.ui.phone.verification;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 
 public class ManualPhoneVerification {
-    private static final List<String> COUNTRIES_REQUIRING_MANUAL_SUBMISSION = Arrays.asList(
+    private static final Set<String> COUNTRIES_REQUIRING_MANUAL_SUBMISSION = new HashSet<>(Arrays.asList(
             "AD", "AR", "AT", "AX", "BA", "BG", "BR", "CD", "CW",
             "DE", "EE", "FI", "GA", "HR", "ID", "IS", "IT", "KH",
             "KR", "LB", "LI", "LR", "MC", "MM", "MY", "NZ", "PA",
-            "RS", "SB", "SO", "TK", "TV", "VA", "WS", "ZA", "ZW");
+            "RS", "SB", "SO", "TK", "TV", "VA", "WS", "ZA", "ZW"));
 
     public static boolean shouldManuallyVerify(Locale locale) {
         return COUNTRIES_REQUIRING_MANUAL_SUBMISSION.contains(locale.getCountry());
