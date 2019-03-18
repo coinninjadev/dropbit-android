b550a1fbb13b2489a8a7e7440177f70b9ec63f19
Mitch Malleo <mitch.malleo@coinninja.com>
2019-03-18 16:10:45 -0400

    Convert DropbitUriBuilder to new interface

--- app/src/main/java/com/coinninja/coinkeeper/util/uri/DropbitUriBuilder.java
+++ app/src/main/java/com/coinninja/coinkeeper/util/uri/DropbitUriBuilder.java
@@ -3,42 +3,26 @@ package com.coinninja.coinkeeper.util.uri;
 import android.net.Uri;
 
 import com.coinninja.coinkeeper.util.uri.parameter.BitcoinParameter;
+import com.coinninja.coinkeeper.util.uri.parameter.DropbitParameter;
 import com.coinninja.coinkeeper.util.uri.routes.DropbitRoute;
 
 import java.util.Map;
 
 import javax.inject.Inject;
 
-public class DropbitUriBuilder extends UrlBuilderInterface<DropbitRoute, BitcoinParameter, Uri> {
-
-    private static final String TOOLTIP_ROUTE = "tooltip";
-    private static final String REGULAR_TRANSACTION_ROUTE = "regulartransaction";
-    private static final String DROPBIT_TRANSACTION_ROUTE = "dropbittransaction";
-    private static final String TRANSACTION_DETAILS = "transactiondetails";
+public class DropbitUriBuilder extends UrlBuilderInterface<DropbitRoute, DropbitParameter, Uri> {
 
     @Override
     public Uri build(DropbitRoute route) {
         Uri.Builder builder = getBuilder()
-                .appendPath(TOOLTIP_ROUTE);
-
-        switch (route) {
-            case TRANSACTION_DETAILS:
-                builder.appendPath(TRANSACTION_DETAILS);
-                break;
-            case REGULAR_TRANSACTION:
-                builder.appendPath(REGULAR_TRANSACTION_ROUTE);
-                break;
-            case DROPBIT_TRANSACTION:
-                builder.appendPath(DROPBIT_TRANSACTION_ROUTE);
-                break;
-        }
+                .appendPath(route.getRoute());
 
         return builder.build();
     }
 
     @Override
-    public Uri build(DropbitRoute route, Map<BitcoinParameter, String> parameters, String... breadcrumbs) {
-        return null;
+    public Uri build(DropbitRoute route, Map<DropbitParameter, String> parameters, String... breadcrumbs) {
+        return build(route);
     }
 
     @Override
@@ -52,7 +36,7 @@ public class DropbitUriBuilder extends UrlBuilderInterface<DropbitRoute, Bitcoin
     }
 
     @Override
-    public Uri build(DropbitRoute route, Map<BitcoinParameter, String> parameters) {
-        return null;
+    public Uri build(DropbitRoute route, Map<DropbitParameter, String> parameters) {
+        return build(route);
     }
 }
--- app/src/main/java/com/coinninja/coinkeeper/util/uri/routes/DropbitRoute.java
+++ app/src/main/java/com/coinninja/coinkeeper/util/uri/routes/DropbitRoute.java
@@ -3,5 +3,18 @@ package com.coinninja.coinkeeper.util.uri.routes;
 public enum DropbitRoute {
     DROPBIT_TRANSACTION,
     REGULAR_TRANSACTION,
-    TRANSACTION_DETAILS
+    TRANSACTION_DETAILS;
+
+    DropbitRoute() {}
+
+    public String getRoute() {
+        switch(this) {
+            case DROPBIT_TRANSACTION:
+                return "tooltip/dropbittransaction";
+            case TRANSACTION_DETAILS:
+                return "tooltip/transactiondetails";
+            case REGULAR_TRANSACTION:
+                return "tooltip/regulartransaction";
+        }
+    }
 }
