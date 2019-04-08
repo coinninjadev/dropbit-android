ef5eb21fac95f8ff4cf104b6e54d3a291a925c43
Jacob Coulter <jacob.coulter@coinninja.com>
2019-04-08 16:50:23 +0000

    Merge branch 'develop' of git.coinninja.net:cn/dropbit-android into primary-currency-tx-detail
    # Conflicts:
    #	app/src/main/java/com/coinninja/coinkeeper/ui/transaction/history/TransactionHistoryActivity.java
    #	app/src/main/java/com/coinninja/coinkeeper/view/widget/DefaultCurrencyDisplayView.java
    #	app/src/main/res/layout/adapter_item_transaction_record.xml
    #	app/src/main/res/layout/merge_default_currency_display_view.xml
    #	app/src/main/res/values/attrs.xml
    #	app/src/test/java/com/coinninja/coinkeeper/ui/transaction/history/TransactionHistoryActivityTest.java
    #	app/src/test/java/com/coinninja/coinkeeper/view/activity/base/BalanceBarActivityTest.java
    #	app/src/test/java/com/coinninja/coinkeeper/view/widget/DefaultCurrencyDisplayViewTest.java

--- app/src/main/java/com/coinninja/coinkeeper/view/animation/StartScreenAnimation.java
+++ app/src/main/java/com/coinninja/coinkeeper/view/animation/StartScreenAnimation.java
@@ -1,9 +1,7 @@
 package com.coinninja.coinkeeper.view.animation;
 
 
-import android.annotation.TargetApi;
 import android.content.res.Resources;
-import android.os.Build;
 import android.transition.ChangeBounds;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
@@ -18,7 +16,6 @@ public class StartScreenAnimation {
 
     private final View relLayFooter;
     private final View txtSlogan;
-    private final View imgLogo;
 
     public StartScreenAnimation(StartActivity activity) {
         this.activity = activity;
@@ -26,11 +23,13 @@ public class StartScreenAnimation {
 
         relLayFooter = activity.findViewById(R.id.start_footer_rellayout);
         txtSlogan = activity.findViewById(R.id.start_txt_slogan);
-        imgLogo = activity.findViewById(R.id.img_logo);
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public void startAnimation_LOLLIPOP() {
+    public void animateOut() {
+        activity.overridePendingTransition(0, 0);
+    }
+
+    public void animateIn() {
         int logoAnimLength = resources.getInteger(R.integer.logo_animation_length_ms);
 
         activity.getWindow().setEnterTransition(null);
@@ -38,22 +37,8 @@ public class StartScreenAnimation {
         bounds.setDuration(logoAnimLength);
         activity.getWindow().setSharedElementEnterTransition(bounds);
 
-        startAnimation(resources);
-    }
-
-    public void startAnimation() {
-        int logoAnimLength = resources.getInteger(R.integer.logo_animation_length_ms);
-
-        AlphaAnimation anim = new AlphaAnimation(0.0f, 1.0f);
-        anim.setDuration(logoAnimLength);
-        imgLogo.startAnimation(anim);
-
-        startAnimation(resources);
-    }
-
-    private void startAnimation(Resources res) {
-        int footerAnimLength = res.getInteger(R.integer.footer_animation_length_ms);
-        int sloganAnimLength = res.getInteger(R.integer.slogan_animation_length_ms);
+        int footerAnimLength = resources.getInteger(R.integer.footer_animation_length_ms);
+        int sloganAnimLength = resources.getInteger(R.integer.slogan_animation_length_ms);
 
         AlphaAnimation animFadeInFooter = new AlphaAnimation(0.0f, 1.0f);
         animFadeInFooter.setDuration(footerAnimLength);
@@ -62,18 +47,6 @@ public class StartScreenAnimation {
         AlphaAnimation animFadeInSlogan = new AlphaAnimation(0.0f, 1.0f);
         animFadeInSlogan.setDuration(sloganAnimLength);
         txtSlogan.startAnimation(animFadeInSlogan);
-    }
-
-    public void animateOut() {
-        activity.overridePendingTransition(0, 0);
-    }
-
-    public void animateIn() {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            startAnimation_LOLLIPOP();
-        } else {
-            startAnimation();
-        }
     }
 
 }
