ed74ccba9b100dc59b2cc71c28693798c6ee0ddd
Michael Lemley <michael.lemley@coinninja.com>
2019-05-13 19:10:44 +0000

    Bug/dimension

--- app/src/main/java/com/coinninja/coinkeeper/factory/DropBitMeUriProvider.java
+++ app/src/main/java/com/coinninja/coinkeeper/factory/DropBitMeUriProvider.java
@@ -2,23 +2,23 @@ package com.coinninja.coinkeeper.factory;
 
 import android.net.Uri;
 
-import com.coinninja.coinkeeper.di.interfaces.DebugBuild;
+import com.coinninja.coinkeeper.di.interfaces.IsProduction;
 
 import javax.inject.Inject;
 
 public class DropBitMeUriProvider {
 
-    private final boolean isDebug;
+    private final boolean isProduction;
 
     @Inject
-    DropBitMeUriProvider(@DebugBuild boolean isDebug) {
-        this.isDebug = isDebug;
+    DropBitMeUriProvider(@IsProduction boolean isProduction) {
+        this.isProduction = isProduction;
     }
 
     public Uri provideUri() {
-        String base = "https://dropbit.me";
-        if (isDebug) {
-            base = "https://test.dropbit.me";
+        String base = "https://test.dropbit.me";
+        if (isProduction) {
+            base = "https://dropbit.me";
         }
         return Uri.parse(base);
     }
